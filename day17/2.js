e3aab1483e7a52e9961f9ef4f25df30f0457ab8e
goodhoopoe <goodhoopoe@gmail.com>
2021-12-18 12:29:22 +0300

    complete day 17

--- day17/2.js
+++ day17/2.js
@@ -5,27 +5,22 @@ const utils = require('../utils');
     data = data
         .split(utils.LINE_BREAK)
         .map(el => el.split(',').map(val => +val));
-    // const x1 = 281,
-    //     x2 = 311,
-    //     y1 = -74,
-    //     y2 = -54;
-    const x1 = 20,
-        x2 = 30,
-        y1 = -10,
-        y2 = -5;
+    const x1 = 281,
+        x2 = 311,
+        y1 = -74,
+        y2 = -54;
 
     let result = 0;
     for (let vx = 0; vx <= x2; vx++) {
-        for (let vy = -y2; vy >= y2; vy--) {
+        for (let vy = -y1; vy >= y1; vy--) {
             let x = 0,
                 y = 0;
             let curX = vx;
             let curY = vy;
-            while (x < x2 && y > y2) {
+            while (x <= x2 && y >= y1) {
                 x += curX;
                 y += curY;
                 if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
-                    console.log(vx, vy, x, y);
                     result++;
                     break;
                 }
