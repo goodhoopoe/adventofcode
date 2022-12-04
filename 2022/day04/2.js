3f696f66ee0bfb75ae4659dae355fe3642d689a9
Ilia Blinov <ilia.blinov@mcmakler.de>
2022-12-04 10:35:40 +0100

    2022 complete day 4

---
+++ 2022/day04/2.js
@@ -0,0 +1,24 @@
+const utils = require('../../utils');
+
+((path, data = utils.readFile(path)) => {
+    console.time();
+    data = data
+        .split(utils.LINE_BREAK)
+        .map(line =>
+            line.split(',').map(block => block.split('-').map(num => +num))
+        );
+
+    var result = 0;
+
+    for (var [[start1, end1], [start2, end2]] of data) {
+        if (
+            (start1 <= start2 && start2 <= end1) ||
+            (start2 <= start1 && start1 <= end2)
+        ) {
+            result++;
+        }
+    }
+
+    console.timeEnd();
+    console.log(result);
+})(process.argv[1]);
