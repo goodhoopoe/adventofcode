0f84a576cbbf117d928918946472be44ba67246b
Ilia Blinov <ilia.blinov@mcmakler.de>
2022-12-09 11:17:28 +0100

    2022 complete day 6

---
+++ 2022/day06/1.js
@@ -0,0 +1,27 @@
+const utils = require('../../utils');
+
+((path, data = utils.readFile(path)) => {
+    console.time();
+
+    var i = 0;
+    var cache = new Set();
+    var result;
+
+    var amountOfDifferentChars = 4;
+
+    while (i < data.length) {
+        if (cache.size === amountOfDifferentChars) {
+            result = i;
+            break;
+        }
+        if (!cache.has(data[i])) {
+            cache.add(data[i]);
+            i++;
+            continue;
+        }
+        cache.delete(data[i - cache.size]);
+    }
+
+    console.timeEnd();
+    console.log(result);
+})(process.argv[1]);
